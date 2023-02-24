eabf1f7a5cef419ad4664a807cae0b6b57fe5797
Tombo-Combo77 <tcousins00@gmail.com>
2023-02-23 19:38:37 -0600

    Updated the pyramid, needs fixed

--- hybrid_images.py
+++ hybrid_images.py
@@ -1,6 +1,6 @@
 """
 Credit: Alyosha Efros
-""" 
+"""
 
 
 import math
@@ -10,6 +10,7 @@ import scipy
 import skimage.transform as sktr
 
 isGray = True
+
 
 def get_points(im1, im2):
     print('Please select 2 points in each image for alignment.')
@@ -21,31 +22,35 @@ def get_points(im1, im2):
     plt.close()
     return (p1, p2, p3, p4)
 
+
 def recenter(im, r, c):
     R, C, _ = im.shape
-    rpad = (int) (np.abs(2*r+1 - R))
-    cpad = (int) (np.abs(2*c+1 - C))
+    rpad = (int)(np.abs(2*r+1 - R))
+    cpad = (int)(np.abs(2*c+1 - C))
     return np.pad(
         im, [(0 if r > (R-1)/2 else rpad, 0 if r < (R-1)/2 else rpad),
              (0 if c > (C-1)/2 else cpad, 0 if c < (C-1)/2 else cpad),
              (0, 0)], 'constant')
+
 
 def find_centers(p1, p2):
     cx = np.round(np.mean([p1[0], p2[0]]))
     cy = np.round(np.mean([p1[1], p2[1]]))
     return cx, cy
 
+
 def align_image_centers(im1, im2, pts):
     p1, p2, p3, p4 = pts
     h1, w1, b1 = im1.shape
     h2, w2, b2 = im2.shape
-    
+
     cx1, cy1 = find_centers(p1, p2)
     cx2, cy2 = find_centers(p3, p4)
 
     im1 = recenter(im1, cy1, cx1)
     im2 = recenter(im2, cy2, cx2)
     return im1, im2
+
 
 def rescale_images(im1, im2, pts):
     p1, p2, p3, p4 = pts
@@ -58,6 +63,7 @@ def rescale_images(im1, im2, pts):
         im2 = sktr.rescale(im2, 1./dscale, channel_axis=2)
     return im1, im2
 
+
 def rotate_im1(im1, im2, pts):
     p1, p2, p3, p4 = pts
     theta1 = math.atan2(-(p2[1] - p1[1]), (p2[0] - p1[0]))
@@ -66,20 +72,22 @@ def rotate_im1(im1, im2, pts):
     im1 = sktr.rotate(im1, dtheta*180/np.pi)
     return im1, dtheta
 
+
 def match_img_size(im1, im2):
     # Make images the same size
     h1, w1, c1 = im1.shape
     h2, w2, c2 = im2.shape
     if h1 < h2:
-        im2 = im2[int(np.floor((h2-h1)/2.)) : -int(np.ceil((h2-h1)/2.)), :, :]
+        im2 = im2[int(np.floor((h2-h1)/2.)): -int(np.ceil((h2-h1)/2.)), :, :]
     elif h1 > h2:
-        im1 = im1[int(np.floor((h1-h2)/2.)) : -int(np.ceil((h1-h2)/2.)), :, :]
+        im1 = im1[int(np.floor((h1-h2)/2.)): -int(np.ceil((h1-h2)/2.)), :, :]
     if w1 < w2:
-        im2 = im2[:, int(np.floor((w2-w1)/2.)) : -int(np.ceil((w2-w1)/2.)), :]
+        im2 = im2[:, int(np.floor((w2-w1)/2.)): -int(np.ceil((w2-w1)/2.)), :]
     elif w1 > w2:
-        im1 = im1[:, int(np.floor((w1-w2)/2.)) : -int(np.ceil((w1-w2)/2.)), :]
+        im1 = im1[:, int(np.floor((w1-w2)/2.)): -int(np.ceil((w1-w2)/2.)), :]
     assert im1.shape == im2.shape
     return im1, im2
+
 
 def align_images(im1, im2):
     pts = get_points(im1, im2)
@@ -89,10 +97,64 @@ def align_images(im1, im2):
     im1, im2 = match_img_size(im1, im2)
     return im1, im2
 
+
 def combine(im1, im2):
+    """Combines two images using simple addition then normalizes them
+
+    Args:
+        im1 (_type_): array corresponding to image 1
+        im2 (_type_): array corresponding to image 2
+
+    Returns:
+        _type_: array containing the finalized output
+    """
     im = im1 + im2
     im = im / im.max()
     return im
+
+
+def gaussianKernel(stdv=.1, mu=0, kernelSz=3):
+    """Returns a gaussian kernel according to the two parameter gaussiam
+    function 1/(2pi*sigma**2) * e**((-x**2+y**2)/(2*sigma**2))
+
+    Args:
+        stdv (float, optional): _description_. Defaults to .1.
+        mu (float, optional): _description_. Defaults to 1.
+        kernelSz (int, optional): _description_. Defaults to 3.
+    Returns:
+        array: array containing the Gaussian Kernel
+    """
+    # creating the kernel and initializing w/ garbage values
+    x, y = np.meshgrid(np.linspace(-1, 1, kernelSz),
+                       np.linspace(-1, 1, kernelSz))
+
+    mag = np.sqrt(x**2+y**2)
+    kernel = np.exp(-((mag-mu)**2 / (2.0 * stdv**2)))
+
+    # need to normalize kernel
+    return kernel/(np.max(kernel))
+
+def hybridImages(im1, im2, stdv=.1, mu=1, kernelSz=3):
+    """This Function Creates a Hybrid Image output given two input images.
+    It does this by convolving a gaussian kernel (for a low pass filter) over 
+    image 1 and the impulse minus the gaussian over image 2, then combining.
+
+    Args:
+        im1 (_type_): Low frequency Image
+        im2 (_type_): High Frequency Image
+        stdv (float, optional): Standard Deviation of the Gaussian filters. Defaults to .1.
+        kernelSz (int, optional): Kernel Size for the gaussian filters. Defaults to 3.
+    """
+
+    # Step One: Forming the kernel for the low and high pass cases
+    lowPass = gaussianKernel(stdv, mu, kernelSz)
+    highPass = 1-lowPass
+
+    lowIm1 = scipy.signal.convolve2d(im1, lowPass)
+    highIm2 = scipy.signal.convolve2d(im2, highPass)
+
+    return combine(lowIm1, highIm2)
+
 
 if __name__ == "__main__":
 
@@ -103,33 +165,38 @@ if __name__ == "__main__":
     im2_name = 'Einstein.jpg'
 
     # 1. load the images
-	
-	# Low frequency image
-    im1 = plt.imread(imageDir + im1_name) # read the input image
-    info = np.iinfo(im1.dtype) # get information about the image type (min max values)
-    im1 = im1.astype(np.float32) / info.max # normalize the image into range 0 and 1
-    
-	# High frequency image
-    im2 = plt.imread(imageDir + im2_name) # read the input image
-    info = np.iinfo(im2.dtype) # get information about the image type (min max values)
-    im2 = im2.astype(np.float32) / info.max # normalize the image into range 0 and 1
-    
+
+    # Low frequency image
+    im1 = plt.imread(imageDir + im1_name)  # read the input image
+    # get information about the image type (min max values)
+    info = np.iinfo(im1.dtype)
+    # normalize the image into range 0 and 1
+    im1 = im1.astype(np.float32) / info.max
+
+    # High frequency image
+    im2 = plt.imread(imageDir + im2_name)  # read the input image
+    # get information about the image type (min max values)
+    info = np.iinfo(im2.dtype)
+    # normalize the image into range 0 and 1
+    im2 = im2.astype(np.float32) / info.max
+
     # 2. align the two images by calling align_images
     im1_aligned, im2_aligned = align_images(im1, im2)
-    
+
     if isGray:
         im1_aligned = np.mean(im1_aligned, axis=2)
         im2_aligned = np.mean(im2_aligned, axis=2)
-	
+
     # Now you are ready to write your own code for creating hybrid images!
     im_low = im1_aligned
     im_high = im2_aligned
-    
-    im = combine(im_low, im_high)
-	
+
+    #im = combine(im_low, im_high)
+    im = hybridImages(im_low, im_high, stdv=.5, mu=0, kernelSz=3)
+    print("here")
     if isGray:
-        plt.imsave(outDir + im1_name[:-4] + '_' + im2_name[:-4] + '_Hybrid.jpg', im, cmap='gray')
+        plt.imsave(outDir + im1_name[:-4] + '_' +
+                   im2_name[:-4] + '_Hybrid.jpg', im, cmap='gray')
     else:
-        plt.imsave(outDir + im1_name[:-4] + '_' + im2_name[:-4] + '_Hybrid.jpg', im)
-    
-    pass
+        plt.imsave(outDir + im1_name[:-4] + '_' +
+                   im2_name[:-4] + '_Hybrid.jpg', im)
--- pyramid_blending.py
+++ pyramid_blending.py
@@ -3,25 +3,106 @@
 # Import required libraries
 import numpy as np
 from matplotlib import pyplot as plt
+from skimage.transform import resize
+# Importing from other files
+# from GetMask import GetMask
+# from hybrid_images import gaussianKernel
 
 # Read source, target and mask for a given id
-def Read(id, path = ""):
+
+
+def Read(id, path=""):
     source = plt.imread(path + "source_" + id + ".jpg")
-    info = np.iinfo(source.dtype) # get information about the image type (min max values)
-    source = source.astype(np.float32) / info.max # normalize the image into range 0 and 1
+    # get information about the image type (min max values)
+    info = np.iinfo(source.dtype)
+    # normalize the image into range 0 and 1
+    source = source.astype(np.float32) / info.max
     target = plt.imread(path + "target_" + id + ".jpg")
-    info = np.iinfo(target.dtype) # get information about the image type (min max values)
-    target = target.astype(np.float32) / info.max # normalize the image into range 0 and 1
-    mask   = plt.imread(path + "mask_" + id + ".jpg")
-    info = np.iinfo(mask.dtype) # get information about the image type (min max values)
-    mask = mask.astype(np.float32) / info.max # normalize the image into range 0 and 1
+    # get information about the image type (min max values)
+    info = np.iinfo(target.dtype)
+    # normalize the image into range 0 and 1
+    target = target.astype(np.float32) / info.max
+    mask = plt.imread(path + "mask_" + id + ".jpg")
+    # get information about the image type (min max values)
+    info = np.iinfo(mask.dtype)
+    # normalize the image into range 0 and 1
+    mask = mask.astype(np.float32) / info.max
 
     return source, mask, target
 
 # Pyramid Blend
-def PyramidBlend(source, mask, target):
-    
-    return source * mask + target * (1 - mask)
+
+
+def PyramidBlend(source, mask, target, **kwargs):
+    """_summary_
+
+    Args:
+        source (_type_): _description_
+        mask (_type_): _description_
+        target (_type_): _description_
+        **kwargs (_type_): Used for overwriting various parameters like gaussian kernel
+
+    Returns:
+        _type_: _description_
+    """
+
+    # These defaults are used for calling the various sub functions
+    defaults = {
+        "Steps": 4
+    }
+    for key in kwargs:
+        if key in defaults:
+            defaults[key] = kwargs[key]
+
+    # Generating the gaussian kernel for the mask
+    # skimage.transform.resize #Already performs gaussian subsampling. Need to make sure anti_aliasing is true
+    # For each gaussian pyramid step we are SMOOTHING THEN subsampling
+    # Prior to this, we need to ensure that the images are of the correct size
+
+    # stepsSource = []  # The laplace at each step of the transform
+    # stepsTarget = []  # The laplace at each step of the transform
+    mergedLaplace = []  # Contains the merged laplacian output for each step
+
+    pM = mask
+    pSource = source
+    pTarget = target
+    for step in range(0, defaults["Steps"]):
+        # Want odd numbers to be larger even numer
+        dim = pM.shape
+        newSize = np.ceil(np.asarray(dim)/2)
+        newSize[-1] = dim[-1]
+
+        # Creating the new images after resizing
+        nM = resize(pM, newSize, anti_aliasing=True)
+        nSource = resize(pSource, newSize, anti_aliasing=True)
+        nTarget = resize(pTarget, newSize, anti_aliasing=True)
+
+        # Upsampling again to determine the Laplacian difference
+        upSource = resize(nSource, pSource.shape)
+        upTarget = resize(nTarget, pTarget.shape)
+
+        # Creating the difference per layer
+        lSource = pSource - upSource
+        lTarget = pTarget - upTarget
+        print(pM.shape, lSource.shape, pSource.shape, "HERE")
+
+        mergedLaplace.append((pM)*lSource+((1-pM)*lTarget))
+
+        pM = nM
+        pSource = nSource
+        pTarget = nTarget
+
+    # Scaling back up
+    # When we are scaling back up we will resize to the size given in the mergedLaplace
+    mergedLaplace = mergedLaplace[::-1]
+    for step in range(0, len(mergedLaplace)-1):
+        scaledUp = resize(mergedLaplace[step], mergedLaplace[step+1].shape)
+        mergedLaplace[step+1] = scaledUp+mergedLaplace[step+1]
+
+    # Normalized to avoid color errors
+    output = mergedLaplace[-1]/np.max(mergedLaplace[-1])
+    print(np.max(output), np.min(output))
+    return output
 
 
 if __name__ == '__main__':
@@ -30,7 +111,6 @@ if __name__ == '__main__':
     outputDir = './Results/'
 
     # main area to specify files and display blended image
-
     index = 1
 
     # Read data and clean mask
@@ -40,14 +120,19 @@ if __name__ == '__main__':
     mask = np.ones_like(maskOriginal)
     mask[maskOriginal < 0.5] = 0
 
-    
+    # For the purposes of this lab ALL IMAGES ARE 300x300
+    # Lazy way of enforcing this rule, takes bottom left of each image
+
+    # mask = mask[:300, :300]
+    # source = source[:300, :300]
+    # target = target[:300, :300]
+
     ### The main part of the code ###
-
     # Implement the PyramidBlend function (Task 2)
-    pyramidOutput = PyramidBlend(source, mask, target)
-    
 
-    
+    pyramidOutput = PyramidBlend(source, mask, target)
+
     # Writing the result
 
-    plt.imsave("{}pyramid_{}.jpg".format(outputDir, str(index).zfill(2)), pyramidOutput)
+    plt.imsave("{}pyramid_{}.jpg".format(
+        outputDir, str(index).zfill(2)), pyramidOutput)
